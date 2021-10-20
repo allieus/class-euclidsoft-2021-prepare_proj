ca6d18dc78cf13dbde358baf7fc308d488b98945
Chinseok Lee <me@askcompany.kr>
2021-10-20 11:13:40 +0900

    memcache/redis 캐싱 설정

--- prepare_proj/settings.py
+++ prepare_proj/settings.py
@@ -90,6 +90,27 @@ DATABASES = {
 }
 
 
+# Cache
+# https://docs.djangoproject.com/ko/3.2/topics/cache/
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.PyMemcacheCache',
+        'LOCATION': '127.0.0.1:11211',
+    }
+}
+
+# CACHES = {
+#     "default": {
+#         "BACKEND": "django_redis.cache.RedisCache",
+#         "LOCATION": "redis://127.0.0.1:6379/1",
+#         "OPTIONS": {
+#             "CLIENT_CLASS": "django_redis.client.DefaultClient",
+#         }
+#     }
+# }
+
+
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
