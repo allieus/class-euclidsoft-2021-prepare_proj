d3a743ec59c86491a93d396bbb9ee42825722e9f
Chinseok Lee <me@askcompany.kr>
2021-10-20 10:28:20 +0900

    Post/Comment/Tag 모델 생성 및 등록

--- app1/models.py
+++ app1/models.py
@@ -1,3 +1,34 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class TimestampedModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Post(TimestampedModel):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    photo = models.ImageField(blank=True)
+    tags = models.ManyToManyField("Tag", blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(TimestampedModel):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    message = models.TextField()
+
+
+class Tag(TimestampedModel):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
