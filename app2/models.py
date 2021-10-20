260f5234c26ccd18ec17d5692d10f54ac78ea9f7
Chinseok Lee <me@askcompany.kr>
2021-10-20 10:36:24 +0900

    app2 Book/BookReview 모델 생성 및 등록

--- app2/models.py
+++ app2/models.py
@@ -1,3 +1,23 @@
+from django.conf import settings
+from django.core.validators import MaxValueValidator
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    cover_photo = models.ImageField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class BookReview(models.Model):
+    store = models.ForeignKey(Book, on_delete=models.CASCADE, related_name="review_set")
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    score = models.PositiveSmallIntegerField(
+        validators=[MaxValueValidator(5)],
+    )
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
