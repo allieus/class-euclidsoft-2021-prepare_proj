d5079ef8b2c5c77302803ed8e75390dd1186ad73
Chinseok Lee <me@askcompany.kr>
2021-10-20 10:30:48 +0900

    app2 장고앱 생성 및 등록

--- prepare_proj/settings.py
+++ prepare_proj/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'accounts',
     'app1',
+    'app2',
 ]
 
 MIDDLEWARE = [
--- prepare_proj/urls.py
+++ prepare_proj/urls.py
@@ -6,6 +6,7 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('app1/', include('app1.urls')),
+    path('app2/', include('app2.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
