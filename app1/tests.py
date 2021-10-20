876e3f75317915c023f521159d08ba70e3a80967
Chinseok Lee <me@askcompany.kr>
2021-10-20 16:40:02 +0900

    pytest 테스트 환경 구성

--- app1/tests.py
+++ app1/tests.py
@@ -1,3 +1,22 @@
-from django.test import TestCase
+import pytest
+from django.contrib.auth import get_user_model
+from app1.models import Post
 
-# Create your tests here.
+
+User = get_user_model()
+
+
+@pytest.fixture
+@pytest.mark.django_db
+def general_user():
+    return User.objects.create(username="user1")
+
+
+@pytest.mark.django_db
+def test_models(general_user):
+    post = Post.objects.create(
+        author=general_user,
+        title="title",
+        content="content",
+    )
+    assert post.pk == 1
