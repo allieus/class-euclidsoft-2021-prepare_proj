8f4eef80c00c8d4c4bf0c6c907d5ff6204a73044
Chinseok Lee <me@askcompany.kr>
2021-10-20 20:24:07 +0900

    diary.Post 모델 생성 및 GeoDjango 연동

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,11 @@ FROM python:3.10
 
 ENV PYTHONUNBUFFERED=1
 
+# https://docs.docker.com/develop/develop-images/dockerfile_best-practices/#sort-multi-line-arguments
+RUN apt update && \
+    apt install -y python3-gdal && \
+    rm -rf /var/lib/apt/lists/*
+
 WORKDIR /code
 COPY requirements.txt /code/
 RUN pip install -r requirements.txt
--- diary/models.py
+++ diary/models.py
@@ -1,3 +1,12 @@
+from django.conf import settings
 from django.db import models
+from django.contrib.gis.db.models import PointField
 
-# Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='+')
+    message = models.TextField()
+    photo = models.ImageField(blank=True)
+    point = PointField(db_index=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
