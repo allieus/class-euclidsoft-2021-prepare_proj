dbebf568cc4996241c3fa8d320163f8bb2cff012
Chinseok Lee <me@askcompany.kr>
2021-10-21 15:54:22 +0900

    diary.Post 썸네일 처리 및 exif 헤더 제거

--- diary/forms.py
+++ diary/forms.py
@@ -1,4 +1,10 @@
+from io import BytesIO
+from typing import Optional
+
+from PIL import Image as PILImage
 from django import forms
+from django.core.files.uploadedfile import UploadedFile
+
 from .models import Post
 
 
@@ -6,3 +12,17 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ["message", "photo"]
+
+    def clean_photo(self):
+        photo: Optional[UploadedFile] = self.cleaned_data.get("photo")
+
+        if photo:
+            pil_image: PILImage = PILImage.open(photo)
+            # 썸네일 처리 및 exif 헤더 제거
+            pil_image.thumbnail((512, 512))
+            thumbnail_photo = BytesIO()
+            thumbnail_photo.name = photo.name
+            pil_image.save(thumbnail_photo)
+            return UploadedFile(thumbnail_photo)
+
+        return photo
